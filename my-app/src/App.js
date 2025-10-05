efdc51240991e1bcb3bc46ca18d8e402fe871a70
AlexBud369 <143994731+AlexBud369@users.noreply.github.com>
2025-10-05 22:32:17 +0300

    feat: add App class component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Catalog from './pages/Catalog/catalog';
+import Home from './pages/Home/home';
+import "./css/index.css";
+import "./css/fonts.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      currentPage: 'home',
+    };
+  }
+
+  switchPage = (page) => {
+    this.setState({currentPage: page });
+  }
+
+  render () {
+    const {currentPage} = this.state;
+    return (
+      <>
+        {currentPage === 'home' ? 
+        (<Home onPageChange = {this.switchPage}/>) : (<Catalog onPageChange = {this.switchPage}/>)}
+      </>
+    );
+  }
 }
 
 export default App;
