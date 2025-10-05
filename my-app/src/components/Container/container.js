9be6839ee500296cb7e6d795c70a4b8142933f8f
AlexBud369 <143994731+AlexBud369@users.noreply.github.com>
2025-10-05 22:29:36 +0300

    feat: add container class component

---
+++ my-app/src/components/Container/container.js
@@ -0,0 +1,45 @@
+import React from "react";
+import CardSection from "../cardSection/cardSection";
+import AsidePanel from "../AsidePanel/asidePanel";
+
+class Container extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        filterOptions: {
+            categories: [
+                'Tops & T-Shirts',
+                'Printed T-Shirts',
+                'Plain T-Shirts',
+                'Kurti',
+                'Boxers',
+            ],
+            colors: [
+                { name: 'purple', hex: 'purple' },
+                { name: 'black', hex: 'black' },
+                { name: 'white', hex: 'white' },
+                { name: 'red', hex: 'red' },
+            ],
+            sizes: ['XS', 'S', 'M', 'L', 'XL'],
+            dressStyles: ['All Styles', 'Classic', 'Casual', 'Formal', 'Sport'],
+            },
+        };
+    }
+
+    render () {
+         const { filterOptions } = this.state;
+         const cardInfo = [
+            {title: "Hawaiian Shirts", brand: "Dress up in summer vibe", price: "50"},
+            {title: "Printed T-Shirt", brand: "New Designs Every Week", price: "45"},
+            {title: "Cargo Joggers", brand: "Move with style & comfort", price: "65"}
+        ];
+        return (
+             <main className="catalog-container">
+                <AsidePanel filterOptions = {filterOptions} />
+                <CardSection cardInfo = {cardInfo}/>
+             </main>
+        );
+    }
+}
+
+export default Container;
