9aef7605df1420bc3b9aa3916f06def6c804a338
AlexBud369 <143994731+AlexBud369@users.noreply.github.com>
2025-10-29 23:51:24 +0300

    fix: correct App component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Catalog from './pages/Catalog/catalog';
+import Home from './pages/Home/home';
+import ErrorPage from "./pages/ErrorPage/errorPage";
+import "./css/index.css";
+import "./css/fonts.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />}/>
+          <Route path="/catalog" element={<Catalog/>} />
+          <Route path="*" element = {<ErrorPage />} />
+        </Routes>
+      </Router>
+    );
 }
 
 export default App;
