d1d4dd18a043cccfc0b975b00e3c674c3e9a123a
AlexBud369 <143994731+AlexBud369@users.noreply.github.com>
2025-12-13 17:36:13 +0300

    feat: add destination routes

---
+++ my-app/backend/routes/destinationRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const destinationController = require('../controllers/destinationController');
+
+router.get('/', destinationController.getAll);
+router.get('/:id', destinationController.getById);
+router.get('/:id/exists', destinationController.exists);
+router.post('/', destinationController.create);
+router.put('/:id', destinationController.update);
+router.delete('/:id', destinationController.delete);
+
+module.exports = router;
