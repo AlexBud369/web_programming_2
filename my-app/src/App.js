ce5c44db669ca33baa19276d0801c6aa68f3c8c4
AlexBud369 <143994731+AlexBud369@users.noreply.github.com>
2025-12-29 18:00:17 +0300

    feat: update App file

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,35 @@
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import { useState } from 'react';
+import { ThemeProvider } from 'styled-components';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Catalog from './pages/Catalog/catalog';
 import Home from './pages/Home/home';
 import ErrorPage from "./pages/ErrorPage/errorPage";
-import "./css/index.css";
-import "./css/fonts.css";
+import GlobalStyles from './styles/GlobalStyles';
+import { lightTheme, darkTheme } from './theme';
 
 function App() {
-    return (
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleTheme = () => {
+    const newTheme = !isDarkMode;
+    setIsDarkMode(newTheme);
+  };
+
+  const currentTheme = isDarkMode ? darkTheme : lightTheme;
+
+  return (
+    <ThemeProvider theme={currentTheme}>
+      <GlobalStyles />
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />}/>
-          <Route path="/catalog" element={<Catalog/>} />
-          <Route path="*" element = {<ErrorPage />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/catalog" element={<Catalog />} />
+          <Route path="*" element={<ErrorPage />} />
         </Routes>
       </Router>
-    );
+    </ThemeProvider>
+  );
 }
 
 export default App;
